840595120802ada757e10eee2071754b69876735
nealstephenson777 <nealstephenson38@hotmail.co.uk>
2022-04-04 17:35:54 +0100

    facilitating for  odd card length

--- main.js
+++ main.js
@@ -1,47 +1,59 @@
 const words = new URLSearchParams(window.location.search);
-
-
 var q = words.get('ccnum')
-var y = q.split('');
-var t = y.splice(-1, 1);
-var z = [...y];
+const y = q.split('');
 
-var f = z.map((element, index) => {
-  if(index % 2 === 0){
-    return element * 2;
-    }
-    return element;
-    });
+const t = y.splice(-1, 1);
 
-var j = f.map(element => {
-  if(element > 9){
-    return element - 9;
-    }
-    return Number(element);
-    });
+const s = t.map(element => Number(element));
 
-var s = t.map(element => Number(element));
-var i = s.reduce((acc, ele) => acc += ele,0);
-var m = j.reduce((acc, ele) => acc += ele,0);
-var w = m % 10;
+const i = s.reduce((acc, ele) => acc += ele,0);
 
-function compareCSum() {
-  if(w === i){
-    return 'true';
+const z = [...y];
+
+
+const f = z.map((element, index) => {
+if(index % 2 === 0){
+  return element * 2;
   }
-  return 'false';
+  return element;
+});
+
+const k = z.map((element, index) => {
+if(index % 2 === 1){
+  return element * 2;
+  }
+  return element;
+});
+
+const checkLength = (z.length % 2 === 1) ? f : k;
+
+const c = checkLength.map(element => (element > 9) ? element - 9 : Number(element));
+
+const m = c.reduce(getSum);
+
+function getSum(total,num){
+  return total + num;
 }
 
+const j = m + i;
 
+const w = j % 10;
 
+function compareCSum() {
+  const comp = (w === 0) ? 'true' : 'false';
+  return comp;
+}
 
 function isValid () {
   if(isNaN(q) || compareCSum() === 'false'){
     return `<p class="shadow">Sorry!</p><br><p class="shadow" >Card number: <span class="shadow" >${q}</span> has failed the checking criteria</p>\n <p class="shadow" > Please check the number and try again!</p>`;
   }
-   return `<p class="shadow" >Hurray!</p><br><p class="shadow" >Card number:<class="shadow" span>${q}</span></p> \n <class="shadow" p>is valid.</p>`;
+     return `<p class="shadow" >Hurray!</p><br><p class="shadow" >Card number:<class="shadow" span>${q}</span></p> \n <class="shadow" p>is valid.</p>`;
 }
 
-  const display = document.getElementById("screen");
-
-  display.innerHTML = isValid();
+const display = document.getElementById("screen");
+display.innerHTML = isValid();
+// const tam = ['4303824040887910']
+// const me = ['5374100200356892']
+// them = ['3529141658564915171']
+//const y = ['3529141658564915171'];
